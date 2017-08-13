469de27cb7604b1785d60d28644f53afd8cf0109
WolvesHu <njzy90@qq.com>
2017-08-13 20:38:01 +0800

    小修改

--- babastore-service-product/src/test/java/com/wolves/demo/test/BaseUnitTest.java
+++ babastore-service-product/src/test/java/com/wolves/demo/test/BaseUnitTest.java
@@ -1,15 +1,15 @@
-//package com.wolves.demo.test;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = {"classpath:application-context.xml"})
-//public class BaseUnitTest {
-//    @Test
-//    public void test() {
-//    	System.out.println("test");
-//    }
-//}
+package com.wolves.demo.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:application-context.xml"})
+public class BaseUnitTest {
+    @Test
+    public void test() {
+    	System.out.println("test");
+    }
+}
--- babastore-service-product/src/test/java/com/wolves/demo/test/TestTbDaoUnitTest.java
+++ babastore-service-product/src/test/java/com/wolves/demo/test/TestTbDaoUnitTest.java
@@ -1,21 +1,21 @@
-//package com.wolves.demo.test;
-//
-//import java.util.Date;
-//
-//import org.junit.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import com.wolves.demo.pojo.TestTb;
-//import com.wolves.demo.service.TestTbService;
-//
-//public class TestTbDaoUnitTest extends BaseUnitTest {
-//	@Autowired
-//	private TestTbService service;
-//	@Test
-//	public void testAdd() {
-//		TestTb testTb = new TestTb();
-//		testTb.setBirthday(new Date());
-//		testTb.setName("������1");
-//		service.addTestTb(testTb );
-//	}
-//}
+package com.wolves.demo.test;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.wolves.demo.pojo.TestTb;
+import com.wolves.demo.service.TestTbService;
+
+public class TestTbDaoUnitTest extends BaseUnitTest {
+	@Autowired
+	private TestTbService service;
+	@Test
+	public void testAdd() {
+		TestTb testTb = new TestTb();
+		testTb.setBirthday(new Date());
+		testTb.setName("顶顶顶");
+		service.addTestTb(testTb );
+	}
+}
